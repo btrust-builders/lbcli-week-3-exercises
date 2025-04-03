4bcc9ede4417519060e6dbe94ad7d35b6179789f
Juwon <ogunseyemicheal@gmail.com>
2025-04-03 14:29:07 +0100

    Updated submission scripts for week 3 exercises

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,4 @@
 # Create a native segwit address and get the public key from the address.
+bitcoin-cli -regtest loadwallet "btrustwallet"
+bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32
+
