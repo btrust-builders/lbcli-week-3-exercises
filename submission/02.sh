902aa5160724e98400748bbad6c12ecfffb59a5d
Juwon <ogunseyemicheal@gmail.com>
2025-04-03 15:38:25 +0100

    corrected the issues

--- submission/02.sh
+++ submission/02.sh
@@ -1,4 +1,3 @@
 # Create a native segwit address and get the public key from the address.
-bitcoin-cli -regtest loadwallet "btrustwallet"
 bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" bech32
 
